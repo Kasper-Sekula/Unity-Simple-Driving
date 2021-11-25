d2909f3ce7cab036a9c1d8a62d6bf3932143b018
Kasper Sekuła <kasper.sekula@gmail.com>
2021-11-25 17:57:23 +0100

    Added steering.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -6,10 +6,21 @@ public class Car : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
     [SerializeField] float speedGainPerSecond = 0.001f;
+    [SerializeField] float turnSpeed = 200f;
+
+    private int steerValue;
 
     void Update()
     {
         speed += speedGainPerSecond * Time.deltaTime;
+
+        transform.Rotate(0, steerValue * turnSpeed * Time.deltaTime, 0f);
+
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
     }
 }
