5dc22c9820d243a88aee18a1a994ab1c82d13d72
Kasper Sekuła <kasper.sekula@gmail.com>
2021-11-27 17:44:18 +0100

    Added highscore system.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] TMP_Text highScoreText;
+
+    private void Start()
+    {
+        int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0); 
+        highScoreText.text = $"HighScore: {highScore.ToString()}";
+
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
